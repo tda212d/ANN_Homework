3d793aad1e90dc09594156b69d064ff40886e306
tda212d <149651263+tda212d@users.noreply.github.com>
2026-02-24 18:01:14 +0700

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-html, body, [class*="css"], [class*="st-"]  {
+html, body, [class*="css"], [class*="st-"], h1, h2, h3, h4, h5, h6 {
     font-family: 'Kanit', sans-serif !important;
 }
 </style>
@@ -130,4 +130,5 @@ if uploaded_file is not None:
 else:
 
     st.info(" โปรดลากไฟล์ CSV มาวาง หรือคลิกที่ปุ่ม Browse files เพื่อเริ่มต้น")
+
 
